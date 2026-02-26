0240c3b164ea6d08864f06d5f87e83d184e590b2
tmjinformatikautm <tmj.informatikautm@gmail.com>
2026-02-26 16:41:22 +0700

    change logo jai

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -18,8 +18,8 @@ export default function Header() {
       <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 py-4">
         <div className="flex items-center justify-between">
           <div className="flex items-center gap-3">
-            <div className="w-12 h-12 bg-blue-600 rounded-full flex items-center justify-center shadow-md">
-              <img src="logo/logo-multimedia.png" alt="Logo Multimedia" />
+            <div className="w-12 h-12">
+              <img src="logo/logo-jai.png" alt="Logo Lab jai" />
             </div>
             <div>
               <h1 className="text-xl md:text-2xl font-bold bg-blue-600 bg-clip-text text-transparent">
